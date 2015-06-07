9d662535d9d6246db38f48e62c4ca2001baccfe0
Nick Manna <manna422@gmail.com>
2015-06-06 23:28:19 -0400

    add file scraper for sample datasheet
    scraping variable file given to contestants

--- src/main.py
+++ src/main.py
@@ -2,24 +2,42 @@ from table import Table
 from gui import Gui
 from gevent import spawn
 
+def read_datasheet(filename):
+    data = {}
+    data['balls'] = []
+    with open(filename) as file:
+        lines = file.readlines()
+        for line in lines:
+            if line.startswith('#'):
+                continue
+            elif line.startswith('['):
+                ball_pos = line[1:-2].split(',')
+                data['balls'].append((int(ball_pos[0]),
+                                      int(ball_pos[1]),
+                                      int(ball_pos[2])))
+            elif '=' in line:
+                entry = line.split('=')
+                try:            
+                    data[entry[0]] = float(entry[1])
+                except Exception, e:
+                    print 'invalid datasheet: %s' % e
+    return data
+        
+
+def read_player_data():
+    pass
+
 def main():
     print 'Simulation started.'
 
     # Test initial table
-    # TODO: replace with values from input file scraper
-    tbl = Table(2700, (2700*2), 50, 100, 25)
-    # for i in range (0, 10000):
-    #     tbl.add_ball(i, i, i)
-
-    tbl.add_ball(1000, 1000, 1)
+    table_data = read_datasheet('../mdata/sample_table.data')
+    tbl = Table(table_data)
 
     spawn(tbl.loop)
 
     gui = Gui(tbl, 400, 800)
     gui.start()
-    for t_ball in tbl.balls:
-        print t_ball.x_pos
-    print len(tbl.balls)
 
 if __name__ == '__main__':
     main()
--- src/table.py
+++ src/table.py
@@ -4,17 +4,21 @@ from physics import *
 from gevent import sleep
 
 class Table(object):
-    def __init__(self, width, length, b_radius,
-        pocket_radius, pocket_offset):
-        self.width = width
-        self.length = length
-        self.b_radius = b_radius
+    def __init__(self, table_data):
+        self.width = table_data['width']
+        self.length = table_data['length']
+        self.b_radius = table_data['b_radius']
+        self.alpha = table_data['alpha']
+        self.dt = table_data['dt']
+
+        self.pocket_radius = table_data['pocket_radius']
+        self.pocket_offset = table_data['pocket_offset']
+
         self.balls = []
-
-        self.pocket_radius = pocket_radius
-        self.pocket_offset = pocket_offset
-
         self.total_score = 0
+
+        for ball in table_data['balls']:
+            self.add_ball(*ball)
 
     def add_ball(self, x_pos, y_pos, score):
         # check if bal placed within table contrainsts
@@ -35,9 +39,6 @@ class Table(object):
         ball.active = False
 
     def loop(self):
-        dt = 0.1
-        alpha = 0.000001
-
         #test
         self.balls[0].x_vel = 100
         self.balls[0].y_vel = 200
@@ -46,14 +47,14 @@ class Table(object):
             # update position, vel, accel
             for ball in self.balls:
                 if not ball.active: continue
-                ball.x_pos += ball.x_vel*dt
-                ball.y_pos += ball.y_vel*dt
+                ball.x_pos += ball.x_vel*self.dt
+                ball.y_pos += ball.y_vel*self.dt
 
-                ball.x_vel += ball.x_acc*dt
-                ball.y_vel += ball.y_acc*dt
+                ball.x_vel += ball.x_acc*self.dt
+                ball.y_vel += ball.y_acc*self.dt
 
-                ball.x_acc = (-1)*(ball.x_vel ** 2)*alpha
-                ball.y_acc = (-1)*(ball.y_vel ** 2)*alpha
+                ball.x_acc = (-1)*(ball.x_vel ** 2)*self.alpha
+                ball.y_acc = (-1)*(ball.y_vel ** 2)*self.alpha
 
             balls_to_check = self.balls[:]
 
@@ -72,4 +73,4 @@ class Table(object):
                     ball.check_collision(b_other)
 
             print "x: %d y: %d" % (self.balls[0].x_pos, self.balls[0].y_pos)
-            sleep(dt)
+            sleep(self.dt)
